bfa9c941c83568da9050c489010fff5b316f70fd
DaylinYP <121044734+DaylinYP@users.noreply.github.com>
2025-04-04 22:52:21 -0600

    Update SpCreateRol.sql
    SpCrear

--- store_procedure/roles/SpCreateRol.sql
+++ store_procedure/roles/SpCreateRol.sql
@@ -1,14 +1,9 @@
-CREATE PROCEDURE SpCreateRol
-    @IdRol INT OUTPUT,
-    @Nombre VARCHAR(25),
-    @Sueldo DECIMAL(10,2)
+CREATE OR ALTER PROCEDURE InsertarRol
+    @Nombre VARCHAR(50),
+    @Sueldo FLOAT
 AS
 BEGIN
     INSERT INTO Roles (Nombre, Sueldo)
     VALUES (@Nombre, @Sueldo)
-
-    SET @IdRol = SCOPE_IDENTITY();
-
-    SELECT @IdRol AS IdRol
-    RETURN
 END
+GO
